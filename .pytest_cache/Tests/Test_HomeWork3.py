76660bd918600f0bc66d887b1b9de2d950aeab99
AnastasiiaVint <a.vintrovich@gmail.com>
2020-09-22 14:02:46 +0300

    HW3 - first test

--- .pytest_cache/Tests/Test_HomeWork3.py
+++ .pytest_cache/Tests/Test_HomeWork3.py
@@ -47,3 +47,51 @@ class Test_homework():
                 sub_item.click()
                 header = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class = 'panel-heading']")))
                 assert header.is_displayed()
+
+        driver.quit()
+
+    def test_second(self):
+        baseUrl = "http://35.236.6.102/litecart/"
+        driver = webdriver.Chrome()
+
+        driver.maximize_window()
+        driver.get(baseUrl)
+
+        first_product = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, " //*[@id='box-campaign-products']//article[1]")))
+
+        first_product_name = first_product.find_element(By.XPATH, ".//*[@class = 'name']").text
+
+        first_product_price = first_product.find_element(By.XPATH, ".//*[@class = 'regular-price']")
+        first_product_price_value = first_product_price.text
+        first_product_price_color = first_product_price.value_of_css_property('color')
+        first_product_price_style = first_product_price.value_of_css_property('text-decoration-line')
+
+        first_product_discount = first_product.find_element(By.XPATH, ".//*[@class = 'campaign-price']")
+        first_product_discount_value = first_product_discount.text
+        first_product_discount_color = first_product_discount.value_of_css_property('color')
+        first_product_discount_style = first_product_discount.value_of_css_property('font-weight')
+
+        first_product.click()
+
+        item_page_product_name = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//*[@id='box-product']//h1"))).text
+        item_page_product_price = driver.find_element(By.XPATH, ".//*[@class = 'regular-price']")
+        item_page_price_value = item_page_product_price.text
+        item_page_price_color = item_page_product_price.value_of_css_property('color')
+        item_page_price_style = item_page_product_price.value_of_css_property('text-decoration-line')
+
+        item_page_discount = driver.find_element(By.XPATH, ".//*[@class = 'campaign-price']")
+        item_page_discount_value = item_page_discount.text
+        item_page_discount_color = item_page_discount.value_of_css_property('color')
+        item_page_discount_style = item_page_discount.value_of_css_property('font-weight')
+
+        assert first_product_name == item_page_product_name
+
+        assert first_product_price_value == item_page_price_value
+        assert first_product_price_color == item_page_price_color
+        assert first_product_price_style == item_page_price_style
+
+        assert first_product_discount_value == item_page_discount_value
+        assert first_product_discount_color == item_page_discount_color
+        assert first_product_discount_style == item_page_discount_style
+
+        driver.quit()
